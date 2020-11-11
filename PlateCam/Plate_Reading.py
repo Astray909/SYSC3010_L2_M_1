7a1d81539e04be1ffcf4811aa1869c8c9bb2b574
Astray909 <justin9.ij@gmail.com>
2020-11-10 20:16:52 -0500

    updated camera code

--- PlateCam/Plate_Reading.py
+++ PlateCam/Plate_Reading.py
@@ -5,14 +5,15 @@ import json
 from openalpr import Alpr
 
 with picamera.PiCamera() as camera:
-    camera.resolution = (2592, 1944)
+    camera.resolution = (1024, 768)
     camera.start_preview()
     # Camera warm-up time
     time.sleep(2)
     camera.capture('plate.jpg')
 
     script_dir = os.path.dirname(__file__) #<-- absolute dir the script is in
-    rel_path = "test.jpg"
+    #rel_path = "test2.jpg"
+    rel_path = "plate.jpg"
     abs_file_path = os.path.join(script_dir, rel_path)
 
     alpr = Alpr("us", "/etc/openalpr/openalp.conf", "/usr/share/openalpr/runtime_data")
@@ -20,6 +21,13 @@ with picamera.PiCamera() as camera:
         print("Error loading OpenALPR")
         sys.exit(1)
     results = alpr.recognize_file(abs_file_path)
-    print(results["results"][0]["plate"])
-    print(results["results"][0]["confidence"])
+    #print(json.dumps(results, indent = 4))
+    try:
+        print(results["results"][0]["plate"])
+    except IndexError:
+        print("No plate found")
+    try:
+        print(results["results"][0]["confidence"])
+    except IndexError:
+        print("Please Try Again")
     alpr.unload()
