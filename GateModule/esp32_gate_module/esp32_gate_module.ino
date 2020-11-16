5f590990882a05bd75230dc68a914f29922f5927
Ezra Pierce <ezrapierce@cmail.carleton.ca>
2020-11-16 15:07:41 -0500

    Gate module updated with ThingSpeak read and write capabilites

--- GateModule/esp32_gate_module/esp32_gate_module.ino
+++ GateModule/esp32_gate_module/esp32_gate_module.ino
@@ -1,20 +1,37 @@
+#include "WiFi.h"
+#include <HTTPClient.h>
+
 #define TRIG_PIN 2
 #define ECHO_PIN 5
+#define SSID "triplex09"
+#define PWD 
+#define SERVER "api.thingspeak.com"
+#define THRESHOLD 30
 
 long duration;
 int distance;
+String API_KEY = "UPJ636UNXXEE2IIG";
+WiFiClient client;
 
 void setup() {
-  pinMode(TRIG_PIN, OUTPUT); 
+  pinMode(TRIG_PIN, OUTPUT);
   pinMode(ECHO_PIN, INPUT);
-  Serial.begin(115200); 
+  Serial.begin(115200);
+
+  WiFi.begin(SSID, PWD);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("Wifi Connected!");
 }
 
 void loop() {
   // Clears the trigPin
   digitalWrite(TRIG_PIN, LOW);
   delayMicroseconds(2);
-  
+
   // Trigger sensor
   digitalWrite(TRIG_PIN, HIGH);
   delayMicroseconds(10);
@@ -22,10 +39,22 @@ void loop() {
 
   // read response
   duration = pulseIn(ECHO_PIN, HIGH);
-  distance= duration*0.034/2;
+  distance = duration * 0.034 / 2;
 
-  Serial.print("Distance: ");
-  Serial.println(distance);
-  delayMicroseconds(1000);
+  // Serial.print("Distance: ");
+  // Serial.println(distance);
+  if (distance > THRESHOLD) {
+    HTTPClient http;
+    String serverPath = "http://api.thingspeak.com/update?api_key=" + API_KEY;
+    http.begin(serverPath.c_str());
+    //http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+    //String httpRequestData = "?api_key=" + API_KEY + "&field3=" + String(1);
+    //Serial.println(httpRequestData);
+    String value = "&field3=A1";
+    int httpResponseCode = http.POST(value);
+    Serial.println(httpResponseCode);
+    http.end();
+  }
+  delay(5000);
 
 }
